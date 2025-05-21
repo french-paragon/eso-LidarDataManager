75babd6bcd96b49af53a149b50458466c7a8243f
french-paragon <french.paragon@gmail.com>
2025-05-21 11:26:46 +0200

    Add license before public sharing

--- processingBlocks/attributesetbasedselector.h
+++ processingBlocks/attributesetbasedselector.h
@@ -1,6 +1,23 @@
 #ifndef ATTRIBUTESETBASEDSELECTOR_H
 #define ATTRIBUTESETBASEDSELECTOR_H
 
+/*
+ * This file is part of the LidarDataManager tool.
+ * Copyright (c) 2025 Laurent Valentin Jospin <laurent.jospin@epfl.ch>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <StereoVision/io/pointcloud_io.h>
 
 #include "identityprocessor.h"
