2a5fe2feb2629b173d76cfd5b0b28953fa93377e
french-paragon <french.paragon@gmail.com>
2025-10-23 14:01:42 +0200

    Use cmake fetchcontent to get external libs
    This remove the need to build them by hand in dockers container
    building, and also remove the need to update the build image on the CI
    computer.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:22.04
+FROM ubuntu:24.04
 
 COPY docker/setup_script.bash /
 RUN bash setup_script.bash
--- docker/setup_script.bash
+++ docker/setup_script.bash
@@ -1,11 +1,1 @@
 apt update; DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install -y build-essential cmake git libeigen3-dev libproj-dev libtclap-dev cimg-dev libgtest-dev libbenchmark-dev
-
-mkdir buildcpp; cd buildcpp;
-
-git clone https://github.com/french-paragon/MultidimArrays.git; mkdir build-multidim; cd build-multidim; cmake -DCMAKE_BUILD_TYPE=Release ../MultidimArrays; make; make install; cd ..
-git clone https://github.com/french-paragon/LibStevi.git; mkdir build-stevi; cd build-stevi; cmake -DCMAKE_BUILD_TYPE=Release -DbuildTests=OFF -DbuildExamples=OFF -DbuildGui=OFF ../LibStevi; make; make install; cd ..
-
-cd ..
-rm -rf buildcpp #cleanup after build;
-
-apt remove -y git #we do not need git
